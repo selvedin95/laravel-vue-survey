a7352bcd85e412f84a6a47608fbd4c3e405661ef
Selvedin <selvedin9595@gmail.com>
2024-09-24 17:07:20 +0200

    novo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:sanctum')->get('/user', function ($request){
+    return $request->user();
 });
+Route::post('/register', [AuthController::class, 'register']);
